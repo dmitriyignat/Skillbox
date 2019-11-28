1e26eaaed317a57d37eca16a91cca355d704d3f8
dmitriyignat <dimitriy.ignit@gmail.com>
2019-11-28 14:36:38 +0300

    First task

--- src/Loader.java
+++ src/Loader.java
@@ -1,10 +1,70 @@
 
 public class Loader
 {
-    public static void main(String[] args)
-    {
-        Cat cat = new Cat();
+    public static void main(String[] args) {
+        /*
+         * Урок 1. Понятие класса и объекта
+         * Создать 5-7 кошек и повызывать у них различные методы:
+         * посмотреть, какой у них вес
+         * попробовать их покормить и посмотреть, что будет с весом
+         * попробовать перекормить кошку, чтобы она взорвалась
+         * "замяукать" кошку до смерти
+         *
+         * Здесь лучше использовать массив. Но так как он выходит за рамки урока
+         *  создается просто 6 кошек и проверяюься их методы.
+         * Чтобы избежать дублирования кода создается метод для демонстрации веса котов.
+         * */
+        Cat vasyka = new Cat();
+        Cat matroskin = new Cat();
+        Cat murzik = new Cat();
+        Cat chernish = new Cat();
+        Cat murka = new Cat();
+        Cat gav = new Cat();
 
-        System.out.println(cat.getStatus());
+        showWeightChange(vasyka);
+
+        showWeightChange(matroskin);
+
+        showWeightChange(murzik);
+
+        showWeightChange(chernish);
+
+        showWeightChange(murka);
+
+        showWeightChange(gav);
+
+        //Взорвали матроскина
+        System.out.println(matroskin.getStatus());
+        matroskin.feed(matroskin.getWeight() * 1000);
+        System.out.println(matroskin.getStatus());
+        System.out.println("");
+
+        //Взорвали мурку
+        System.out.println(murka.getStatus());
+        murka.drink(murka.getWeight() * 1000);
+        System.out.println(murka.getStatus());
+        System.out.println("");
+
+        //Замяукали гава
+        System.out.println(gav.getStatus());
+        while (gav.getWeight() > 1000) {
+            gav.meow();
+        }
+        System.out.println(gav.getStatus());
+    }
+
+
+
+    /*
+     * Так как метод main статический он не может вызывать только статические методы.
+     * Соответственно созданный метод showWeightChange - статический
+     * */
+    private static void showWeightChange(Cat cat) {
+
+        System.out.println(cat.getWeight());
+        cat.feed(cat.getWeight() / 100);
+        System.out.println(cat.getWeight());
+        System.out.println("");
+
     }
 }
